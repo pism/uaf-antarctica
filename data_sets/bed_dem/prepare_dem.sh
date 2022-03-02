245e7cf7c14437b52fd3004c6796c482e10377b8
Kyle Blum <kblum2@alaska.edu>
2022-03-02 14:54:38 -0900

    remove floating ice from velocity fields

--- data_sets/bed_dem/prepare_dem.sh
+++ data_sets/bed_dem/prepare_dem.sh
@@ -25,6 +25,10 @@ for grid in 16000 8000 4000 2000 1000 500; do
         echo "Preparing weights for ${grid}m grid"
         cdo -f nc4 -z zip_2 -P $NN genycon,../grids/g${grid}m.txt $infile machine_weights_${grid}m.nc
     fi
+    if [ ! -f nn_machine_weights_${grid}m.nc ]; then
+        echo "Preparing weights for ${grid}m grid"
+        cdo -f nc4 -z zip_2 -P $NN gennn,../grids/g${grid}m.txt $infile nn_machine_weights_${grid}m.nc
+    fi
     if [ ! -f map_weights_${grid}m.nc ]; then
         echo "Preparing weights for ${grid}m grid"
         cdo -f nc4 -z zip_2 -P $NN genycon,../grids/g${grid}m.txt bedmap2_bed.nc map_weights_${grid}m.nc
@@ -39,7 +43,10 @@ done
 for grid in 16000 8000 4000 2000 1000 500; do
     echo "Preparing ${grid}m grid"
     outfile=pism_BedMachineAntarctica_v01_g${grid}m.nc
-    cdo -f nc4 -z zip_2 -P $NN remap,../grids/g${grid}m.txt,machine_weights_${grid}m.nc $infile $outfile
+    cdo -f nc4 -z zip_2 -P $NN remap,../grids/g${grid}m.txt,machine_weights_${grid}m.nc -delname,mask,source $infile float.nc   # float vars
+    cdo -f nc4 -z zip_2 -P $NN remap,../grids/g${grid}m.txt,nn_machine_weights_${grid}m.nc -selname,mask,source $infile byte.nc # byte vars
+    cdo merge float.nc byte.nc $outfile
+    rm byte.nc float.nc
     # Make FTT mask: 1 where there is no floating (3) or grounded (2) ice
     ncap2 -O -s "where(thickness<0) thickness=0; ftt_mask[\$y,\$x]=0b; where(mask==0) {thickness=0.; surface=0.;}; where(mask!=2) ftt_mask=1; where(mask!=3) ftt_mask=1;" $outfile $outfile
     ncap2 -O -s 'land_ice_area_fraction_retreat = thickness; where(thickness > 0 || thickness + bed >= (1 - 910.0/1028.0) * thickness + 0) land_ice_area_fraction_retreat = 1;land_ice_area_fraction_retreat@units="1";land_ice_area_fraction_retreat@long_name="maximum ice extent mask";land_ice_area_fraction_retreat@standard_name="";' $outfile $outfile
--- data_sets/velocities/prepare_velocities.sh
+++ data_sets/velocities/prepare_velocities.sh
@@ -13,6 +13,7 @@ done
 
 for grid in 16000 8000 4000 2000 1000 500; do
     echo "Preparing ${grid}m grid"
-    outfile=antarctica_ice_velocity_g${grid}m.nc
-    cdo -L -f nc4 -z zip_2 -P $NN remap,../grids/g${grid}m.txt,sar_weights_${grid}m.nc -chname,magnitude,velsurf_mag -selvar,magnitude $infile $outfile
+    outfile=antarctica_ice_velocity_g${grid}m
+    cdo -L -f nc4 -z zip_2 -P $NN setctomiss,0 -remap,../grids/g${grid}m.txt,sar_weights_${grid}m.nc -chname,magnitude,velsurf_mag -selvar,magnitude $infile ${outfile}.nc
+    python add_coord.py $outfile ../bed_dem/pism_BedMachineBedmapHFAntarctica_v01_g${grid}m.nc mask 2,4 -o ${outfile}_no_shelves.nc
 done
