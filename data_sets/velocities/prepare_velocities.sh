b12c4393a34b4c974585be915457e6d6ef215301
Andy Aschwanden <andy.aschwanden@gmail.com>
2022-12-15 11:09:34 -0900

    updated best pseudo-plastic from MCMC

--- data_sets/velocities/prepare_velocities.sh
+++ data_sets/velocities/prepare_velocities.sh
@@ -4,9 +4,9 @@ NN=4
 infile=antarctica_ice_velocity_450m_cf.nc
 
 for grid in 16000 8000 4000 2000 1000 500; do
-    if [ ! -f sar_weights_${grid}m.nc ]; then
+    if [ ! -f 2sar_weights_${grid}m.nc ]; then
         echo "Preparing weights for ${grid}m grid"
-        cdo -f nc4 -z zip_2 -P $NN genycon,../grids/g${grid}m.txt $infile sar_weights_${grid}m.nc
+        cdo -f nc4 -z zip_2 -P $NN genycon,../grids/g${grid}m.txt $infile 2sar_weights_${grid}m.nc
     fi
 done
 
